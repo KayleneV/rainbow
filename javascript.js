03a78f28c9852dd3a0ef1326eab816206e85c4cd
Kaylene Van Wyk <95338328+KayleneV@users.noreply.github.com>
2022-04-07 17:18:54 +0200

    started rainbow

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function redFunction(){
+    let redBox = document.getElementById('red');
+    redBox.style.backgroundColor = 'red';
+    redBox.innerHTML = "I am redBox!"
+}
+
+function orangeFunction(){
+    let orangeBox = document.getElementById('orange');
+    orangeBox.style.backgroundColor = 'orange';
+    orangeBox.innerHTML = "I am orange!"
+}
+
+function greenFunction(){
+    let greenBox = document.getElementById('green');
+    greenBox.style.backgroundColor = 'green';
+    greenBox.innerHTML = "I am green!";
+}
+
+function blueFunction(){
+    let blueBox = document.getElementById('blue');
+    blueBox.style.backgroundColor = 'blue';
+    blueBox.innerHTML = "I am blue";
+}
+
+function indigoFunction(){
+    let indigoBox = document.getElementById('indigo');
+    indigoBox.style.backgroundColor = 'indigo';
+    indigoBox.innerHTML = "I am indigo";
+}
+
+function purpleFunction(){
+    let purpleBox = document.getElementById('purple');
+    purpleBox.style.backgroundColor = 'purple';
+    purpleBox.innerHTML = 'I am purple';
+}
